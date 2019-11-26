0e9290745891fc3b2d455fe4475207ab86b604d4
cecyrhz09 <56567856+cecyrhz09@users.noreply.github.com>
2019-11-25 19:17:57 -0600

    Add files via upload

---
+++ State/waterState.py
@@ -0,0 +1,50 @@
+'''
+Created on 23 nov. 2019
+
+@author: Cecy Rodríguez
+'''
+from abc import ABC, abstractmethod
+class WaterState(ABC):
+
+   name = "state"
+   allowed = []
+
+   def switch(self, state):
+      if state.name in self.allowed:
+         print ('Current:',self,' => switched to new state',state.name)
+         self.__class__ = state
+      else:
+         print ('Current:',self,' => switching to',state.name,'not possible.')
+
+   def __str__(self):
+      return self.name
+
+class Cold(WaterState):
+   name = "cold"
+   allowed = ['hot']
+
+class Hot(WaterState):
+   name = "hot"
+   allowed = ['cold','warm']
+
+class Warm(WaterState):
+   name = "warm"
+   allowed = ['on']
+
+class Water():
+   
+   def __init__(self, model='Osmo'):
+      self.model = model
+      self.state = Cold()
+   
+   def change(self, state):
+      self.state.switch(state)
+
+if __name__ == "__main__":
+   wat = Water()
+   wat.change(Hot)
+   wat.change(Cold)
+   wat.change(Hot)
+   wat.change(Warm)
+   wat.change(Hot)
+   wat.change(Cold)
